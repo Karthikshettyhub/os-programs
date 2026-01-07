71e13306cf6c7ebb1f0b1126fd221501d5e48720
karthikshetty <karthikashetty036@gmail.com>
2026-01-07 18:55:43 +0530

    Refactor process scheduling logic

--- pg2c.c
+++ pg2c.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 
 int main() {
+
     int n, tq;
-    int bt[20], at[20], rt[20];
-    int time = 0, completed = 0;
+    int at[20], bt[20], rt[20];
     int wt = 0, tat = 0;
+    int time = 0, done = 0;
+    int i;
 
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    for(int i = 0; i < n; i++) {
-        printf("Enter arrival time & burst time of P%d: ", i+1);
+    /* ---- Input like FCFS ---- */
+    for(i = 0; i < n; i++) {
+        printf("Enter arrival time & burst time of P%d: ", i + 1);
         scanf("%d %d", &at[i], &bt[i]);
-        rt[i] = bt[i];  // remaining time
+        rt[i] = bt[i];
     }
 
     printf("Enter Time Quantum: ");
@@ -20,34 +23,37 @@ int main() {
 
     printf("\nProcess\tTurnaround Time\tWaiting Time\n");
 
-    while(completed < n) {
-        for(int i = 0; i < n; i++) {
+    /* ---- Scheduling Loop ---- */
+    while(done < n) {
+
+        for(i = 0; i < n; i++) {
 
             if(rt[i] > 0 && at[i] <= time) {
 
                 if(rt[i] > tq) {
-                    time += tq;
-                    rt[i] -= tq;
-                } 
+                    time = time + tq;
+                    rt[i] = rt[i] - tq;
+                }
                 else {
-                    time += rt[i];
+                    time = time + rt[i];
                     rt[i] = 0;
-                    completed++;
+                    done++;
 
-                    int process_tat = time - at[i];
-                    int process_wt = process_tat - bt[i];
+                    int cur_tat = time - at[i];
+                    int cur_wt  = cur_tat - bt[i];
 
-                    tat += process_tat;
-                    wt += process_wt;
+                    tat = tat + cur_tat;
+                    wt  = wt + cur_wt;
 
-                    printf("P%d\t\t%d\t\t%d\n", i+1, process_tat, process_wt);
+                    printf("P%d\t\t%d\t\t%d\n",
+                           i + 1, cur_tat, cur_wt);
                 }
             }
         }
     }
 
-    printf("\nAverage Waiting Time: %.2f", (float)wt/n);
-    printf("\nAverage Turnaround Time: %.2f\n", (float)tat/n);
+    printf("\nAverage Waiting Time: %.2f", (float)wt / n);
+    printf("\nAverage Turnaround Time: %.2f\n", (float)tat / n);
 
     return 0;
 }
